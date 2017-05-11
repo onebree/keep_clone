92a63a2327382f29278df115c317d99130a040c4
Hunter Stevens <onebree@gmail.com>
2017-05-11 16:00:57 -0400

    Add Firebase for auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBE7hjMA8DVOfdj5S5YPK_RmaVfmwHrAWI',
+      authDomain: 'keep-clone-604f2.firebaseapp.com',
+      databaseURL: 'https://keep-clone-604f2.firebaseio.com',
+      projectId: 'keep-clone-604f2',
+      storageBucket: 'keep-clone-604f2.appspot.com',
+      messagingSenderId: '103102107134'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <LoginForm />
