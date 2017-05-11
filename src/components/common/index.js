4ab7f765255f96f188d79dff9f44987651907a88
Hunter Stevens <onebree@gmail.com>
2017-05-11 14:27:39 -0400

    Create common components - card, card stack, field

---
+++ src/components/common/index.js
@@ -0,0 +1,8 @@
+// This strategy allows you to export all components from a folder
+// so they are accessible elsewhere in the app
+// Example:
+//   import { Header } from './components/common';
+
+export * from './Card';
+export * from './CardSection';
+export * from './Field';
