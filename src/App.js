91b367f3a7b9a824a9e97844c518a48effa05aa3
Hunter Stevens <onebree@gmail.com>
2017-05-11 13:34:57 -0400

    Login form boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello world!</Text>
-      </View>
+      <LoginForm />
     );
   }
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Login form</Text>
+      </View>
+    );
+  }
+}
+
+export default LoginForm;
