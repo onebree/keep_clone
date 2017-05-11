8c3e2660b8b9b39ff312dc294d670fba93905fab
Hunter Stevens <onebree@gmail.com>
2017-05-11 14:34:58 -0400

    login form skeleton - fields and button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Button } from 'react-native';
+import { Card, CardSection, Field } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
-      <View>
-        <Text>Login form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Field
+            label="Email"
+            placeholder="test@example.com"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Field
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+          />
+        </CardSection>
+
+        <CardSection>
+          <View style={{ flex: 1 }}>
+            <Button
+              title="Login"
+              onPress={() => console.log('pressed')}
+            />
+          </View>
+        </CardSection>
+      </Card>
     );
   }
 }
